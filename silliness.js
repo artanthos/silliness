c2adba0c04bb3ad38752bf388977da6257a99556
artanthos <alexandru.okros@live.com>
2017-05-30 19:23:20 +0300

    Add files via upload

---
+++ silliness.js
@@ -0,0 +1,15 @@
+var randomNumber = 10;
+
+(function output(arg){
+	
+	arg--;
+	
+	console.log(arg); // you move the console above the recursive call - silly, right?
+	
+	if(arg>1){
+		output(arg);
+	} 	
+	
+	// what do you think the answer is?
+  
+})(randomNumber);
